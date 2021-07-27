822abf92b3bac5914d82efe205b7d4d5c32df717
jiansheng-eb <xujiansheng@easternbay.cn>
2021-07-27 13:56:49 +0800

    add

--- download_test.sh
+++ download_test.sh
@@ -1,1 +1,2 @@
-#!/bin/bashcurl -o /tmp/example.pdf https://hr.simplyhired-dev.com/docs/hris-terms-service.pdf
+#!/bin/bash
+curl -o /tmp/example.pdf https://hr.simplyhired-dev.com/docs/hris-terms-service.pdf
